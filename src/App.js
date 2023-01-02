fdad915c88e537e069915c9b25287387d9b32b24
Ehuntwork <sir.mle.learnsalot@gmail.com>
2023-01-01 19:17:50 -0800

    MVP Met

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import axios from 'axios';
+import React,{useState,useEffect} from 'react';
+import Character from './components/Character'
 
 const App = () => {
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -8,11 +11,29 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data,setData] = useState()
+
+  useEffect(()=>{
+    axios.get(`https://swapi.dev/api/people`)
+    .then(res=>{setData(res.data.results)})
+    .catch(err=>{console.log(err)})
+  },[])
+
+  console.log(data ? data: '...')
+  if(data){
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.map(char=>{
+        return <Character charData={char}/>
+      })}
     </div>
-  );
+  );}
+  else return (
+    <div className="App">
+      <h1 className="Header">Characters</h1>
+    </div>
+  )
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+export const CharDiv = styled.div`
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+ background-color: white;
+ margin: 20px;
+ padding: 20px;
+ border-radius: 20px
+`
+export default function Character(props){
+    const {charData} = props
+    return <CharDiv className="character">
+        <h2>{charData.name}</h2>
+        <p>{charData.birth_year}</p>
+    </CharDiv>
+}
